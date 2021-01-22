ef745b22be3d5ac4d5c542f630e4a765cfcb56f8
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-01-22 10:33:24 -0500

    Making Todos Service Implementation

---
+++ src/main/java/com/lambdaschool/todos/services/TodosServiceImpl.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.todos.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service(value = "todosService")
+public class TodosServiceImpl implements TodosService{
+
+    @Override
+    public void markComplete(long todoid) {
+
+    }
+}
